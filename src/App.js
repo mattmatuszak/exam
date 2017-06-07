2be7d8b2d6f0d77c76d6be666d72448d8f3fa6e7
Matt Matuszak <mattmatuszak@northwesternmutual.com>
2017-06-07 10:57:15 -0500

    search with github for a specific user is working...mostly

--- src/App.js
+++ src/App.js
@@ -2,23 +2,51 @@ import React, {Component} from 'react';
 
 import './App.css'
 
+import axios from 'axios'
+
 import Header from './Header'
 import SearchForm from './SearchForm'
 import Repos from './Repos'
 import RepoDetail from './RepoDetail'
 
 class App extends Component {
+
+    state = {
+          searched: false
+        , githubUser: null
+        , githubUserRepos: []
+    }
+
+    searchGithubUsers = (formInputs) => {
+        console.log('App.searchGithubUsers() formInputs', formInputs);
+
+        axios
+            .get(`https://api.github.com/users/${formInputs.username}`)
+            .then((response) => {
+                console.log('App.searchGithubUsers() response', response.data);
+                this.setState({githubUser: response.data, searched: true})
+            })
+            .catch((error) => {
+                console.log('App.searchGithubUsers() response', error);
+                this.setState({githubUser: null, searched: true})
+            })
+
+    }
+
+
     render() {
         return (
             <div className="App">
 
                 <div className="row">
-                    <Header/>
+                    <Header />
                 </div>
                 <div className="gridContainer viewerContent">
                     <div className="row">
                         <div className="small-12">
-                            <SearchForm/>
+                            <SearchForm
+                                searchGithubUsers={this.searchGithubUsers}
+                                githubUserFound={!(this.state.githubUser === null && this.state.searched)} />
                         </div>
                     </div>
                     <div className="row">
--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,23 +1,55 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types';
 
 class SearchForm extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            username: ''
+        }
+    }
+
+    formChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    submit = (event) => {
+        event.preventDefault()
+        // console.log('submitted')
+        this.props.searchGithubUsers(this.state)
+    }
 
     render() {
         return (
             <div>
-                <div data-notification className="notification-box alert">
-                    Unknown username!
-                    <a href="#" className="close">×</a>
-                </div>
+                {
+                    (!this.props.githubUserFound)
+                    ? (
+                        <div data-notification className="notification-box alert">
+                            Unknown username!
+                            <a href="#" className="close">×</a>
+                        </div>
+                    )
+                    : null
+                }
                 <p>Search repositories by username</p>
-                <form>
-                    <input type='text' placeholder='username'/>
-                    <button type='submit' className="tiny expand">Search</button>
+                <form onSubmit={this.submit}>
+                    <input type='text' placeholder='username' name="username" onChange={this.formChange} />
+                    <button type='submit' className="tiny expand" disabled={(this.state.username === '')}>Search</button>
                 </form>
                 <hr/>
             </div>
         )
     }
+}
+
+SearchForm.propTypes = {
+    searchGithubUsers: PropTypes.func.isRequired
+    , githubUserFound: PropTypes.bool.isRequired
 }
 
 export default SearchForm
